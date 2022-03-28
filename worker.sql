490f13912c2a53542a9a305f81f3cd4a10b3eb74
Oxana Lezhneva <pythonpride@ya.ru>
2022-03-28 13:48:50 +0300

    Update worker.sql

--- worker.sql
+++ worker.sql
@@ -1,1 +1,5 @@
-create table if not exists Worker (Id serial primary key, Name varchar(100) not null, Departament integer not null, Boss_id integer not null references Worker(Id));
+create table if not exists Worker (
+  Id serial primary key, 
+  Name varchar(100) not null, 
+  Departament integer not null, 
+  Boss_id integer not null references Worker(Id));
